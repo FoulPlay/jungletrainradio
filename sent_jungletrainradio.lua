4d92749f41b03d8b32ef4447946405f769b85a2f
Nathan Binks <FoulPlay@users.noreply.github.com>
2017-02-19 19:05:58 +0000

    Update 6
    +Added console command to allow playing of the stream (The audio from the radio.) DEFAULT is 1
    +Added console command to enable debugging. DEFAULT is 0
    *Made "jtr_enable" console command pauses the stream (when set to 0) or plays the steam (when set to 1)
    *Made "jtr_debug" shows debugging information in the console (when set to 1) or don't show debugging information (when set to 0)
    *Format corrections
    
    Version 1.3.3

--- sent_jungletrainradio.lua
+++ sent_jungletrainradio.lua
@@ -1,4 +1,4 @@
---[[JungleTrainRadio by Foul Play | Version 1.3.1]]
+--[[JungleTrainRadio by Foul Play | Version 1.3.3]]
 --[[
 	function() end --A function
 	for() do --A loop
@@ -7,6 +7,12 @@
 	else --A condiction
 	elseif then --A condiction
 	a = nil --A var
+	a ~= b -- Relational Operator
+	a == b --Relational Operator
+	a > b -- Relational Operator
+	a < b -- Relational Operator
+	a => b -- Relational Operator
+	a <= b -- Relational Operator
 	a + b = c --Add maths
 	a - b = c --Subtract maths
 	a / b = c --Divide maths
@@ -17,34 +23,36 @@ AddCSLuaFile()
 
 DEFINE_BASECLASS( "base_anim" )
 
-cleanup.Register( "JungleTrain Radio" )
+cleanup.Register( "JungleTrain Radio" ) --Registers for the entity to be cleaned up by Admins and clients.
 
-ENT.PrintName = "JungleTrain Radio"
-ENT.Author = "Nathan Binks"
-ENT.Information = "A spawnable internet radio."
-ENT.Category = "JungleTrain Radio"
-ENT.Spawnable = true
-ENT.AdminOnly = false
-ENT.RenderGroup = RENDERGROUP_TRANSLUCENT
+ENT.PrintName = "JungleTrain Radio" --The name of the entity in the Spawn Menu.
+ENT.Author = "Nathan Binks" --The author of the entity in the Spawn Menu.
+ENT.Information = "A spawnable internet radio." --Information about the entity in the Spawn Menu.
+ENT.Category = "JungleTrain Radio" --The Category where its going to be stored in the Spawn Menu.
+ENT.Spawnable = true --Makes it Spawnable by Clients.
+ENT.AdminOnly = false --Make it so non-admins can spawn it in.
+ENT.RenderGroup = RENDERGROUP_TRANSLUCENT --TODO: Add information about what it does.
 
 local a = { a = nil, b = nil } --Table for channels and radios.
 local b = "http://stream1.jungletrain.net:8000/" --URL for the station.
+CreateClientConVar( "jtr_enable", "1", true, true, "Enable or disable JungleTrain radios stream. DEFAULT 1" ) --Option to pause the stream.
+CreateClientConVar( "jtr_debug", "0", false, false, "Enbale debugging for JungleTrainRadio. DEFAULT 0" ) --Option to enabled the debugging.
 
 local function jtrCreateSound( ent )
-	--Since 'sound.PlayURL' is client side only, 
-	--we use a if statment to make sure it isn't run on the server.
+	--[[Since 'sound.PlayURL' is client side only, 
+	we use a if statment to make sure it isn't run on the server.]]
 	if ( CLIENT ) then
 		if ( IsValid( ent ) ) then
 			sound.PlayURL( b, "3d", function( station )
 				--If valid then add the IGModAudioChannel to the table.
 				if ( IsValid( station ) ) then
-					--station:SetPos( LocalPlayer():GetPos() ) --Set the 3d position to the player's position for debugging.
-					
 					if ( a[ ent:EntIndex() ] == nil ) then
 						a[ ent:EntIndex() ] = { a = ent, b = station } -- Add the station to the 'b' value.
-						
-						print( ent:EntIndex() .. " | " .. ent:GetClass() .. " | Created Channel | " .. tostring( a[ ent:EntIndex() ].b ) ) --Debugging.
-						PrintTable( a ) --Debugging.
+
+						if ( GetConVar( "jtr_debug" ):GetInt() == 1 ) then
+							print( ent:EntIndex() .. " | " .. ent:GetClass() .. " | Created Channel | " .. tostring( a[ ent:EntIndex() ].b ) ) --Debugging.
+							PrintTable( a ) --Debugging.
+						end
 					end
 				else
 					LocalPlayer():ChatPrint( "Invalid URL!" ) --Make sure that the URL is valid.
@@ -55,8 +63,8 @@ local function jtrCreateSound( ent )
 end
 
 local function jtrManageSound()
-	--Since 'sound.PlayURL' is client side only, 
-	--we use a if statment to make sure it isn't run on the server.
+	--[[Since 'sound.PlayURL' is client side only, 
+	we use a if statment to make sure it isn't run on the server.]]
 	if ( CLIENT ) then
 		--Run through the table.
 		for k, v in pairs( a ) do
@@ -70,12 +78,19 @@ local function jtrManageSound()
 					v.b:Play()
 				end
 
+				--If the client disables the stream while the stream is playing then pause the stream.
+				if ( GetConVar( "jtr_enable" ):GetInt() == 0 ) then
+					v.b:Pause()
+					else
+					v.b:Play()
+				end
+
 				v.b:SetPos( v.a:GetPos() ) --Set the stream position in the world to the radio's position in the world.
 
 			--If the entity is valid but the stream isn't then make the key in the table nil.
 			elseif ( v.a:IsValid() and not v.b:IsValid() ) then 
 				a[ k ] = nil
-			
+
 			--If the entity isn't valid but the stream is valid then stop the stream and make the key nil.
 			elseif ( not v.a:IsValid() and v.b:IsValid() ) then
 				v.b:Stop()
@@ -87,8 +102,8 @@ end
 timer.Create( "jtrManageSound", .1, 0, function() jtrManageSound() end )
 
 function ENT:SetupDataTables()
-	self:NetworkVar( "Int", 0, "price" )
-	self:NetworkVar( "Entity", 1, "owning_ent" )
+	self:NetworkVar( "Int", 0, "price" ) --The price of the entity in the F4 Menu in DarkRP.
+	self:NetworkVar( "Entity", 1, "owning_ent" ) --Sets the owner of the entity when spawned by clients to them.
 end
 
 function ENT:SpawnFunction( ply, tr, ClassName )
@@ -108,8 +123,7 @@ function ENT:SpawnFunction( ply, tr, ClassName )
 end
 
 function ENT:Initialize()
-	--Set the model ingame.
-	self:SetModel( "models/props_lab/citizenradio.mdl" )
+	self:SetModel( "models/props_lab/citizenradio.mdl" ) --Set the model ingame.
 	
 	--Enables Physics on Client.
 	self:SetMoveType( MOVETYPE_VPHYSICS )
@@ -118,8 +132,7 @@ function ENT:Initialize()
 	jtrCreateSound( self ) --Run the function to create the stream.
 	
 	if ( SERVER ) then
-		--Only use this Physics on server side or the Physgun beam will fuck up.
-		self:PhysicsInit( SOLID_VPHYSICS )
+		self:PhysicsInit( SOLID_VPHYSICS ) --Only use this Physics on server side or the Physgun beam will fuck up.
 	end
 end
 
@@ -135,10 +148,9 @@ end
 --https://github.com/garrynewman/garrysmod/blob/master/garrysmod/lua/entities/sent_ball.lua#L149
 if ( SERVER ) then return end -- We do NOT want to execute anything below in this FILE on SERVER 
 
-language.Add( "Cleanup_JungleTrain Radio", "JungleTrain Radios" )
-language.Add( "Cleaned_JungleTrain Radio", "Cleaned up JungleTrain Radios" )
+language.Add( "Cleanup_JungleTrain Radio", "JungleTrain Radios" ) --Sets what it says in the cleanup menu in the Spawn Menu.
+language.Add( "Cleaned_JungleTrain Radio", "Cleaned up JungleTrain Radios" ) --Sets what it says when the entity gets cleaned.
 
 function ENT:Draw()
-	--Drawing the model
-	self:DrawModel()
+	self:DrawModel() --Draw the model.
 end
